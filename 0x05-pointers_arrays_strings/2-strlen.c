f431a8beb75f0969faa087f0dbfbccf9d4a0fe24
Joe444-prog <Joshuagbenoba00@gmail.com>
2022-04-21 15:52:24 -0700

    T2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * _strlen - calculate the length of a string.
- *
- * @s: the string to calculate it's length.
+ * _strlen - calculate the length of a string. 
+ * @s: the string to calculate it's length.     
  * Return: length of a string.
  */
 
