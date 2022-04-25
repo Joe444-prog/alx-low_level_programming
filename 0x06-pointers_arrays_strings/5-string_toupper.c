ed0602615bd104a498ec944fcb4dfea14588ba41
Joe444-prog <Joshuagbenoba00@gmail.com>
2022-04-25 06:45:03 -0700

    S-toupper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,14 +1,16 @@
 include "main.h"
 
 /**
- * string_toupper - changes all lowercase letters of a string to uppercase.
+ * string_toupper - changes lowercase to uppercase.
  * @s: string to modify.
  * Return: s modified.
  */
 
 char *string_toupper(char *str)
 {
-	int i = 0;
+	int i;
+	
+	i = 0;
 
 	while (str[i] != '\0')
 	{
