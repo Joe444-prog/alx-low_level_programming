f6fbeae5519d1c7e00978dc3aba564843c9e3797
Joe444-prog <Joshuagbenoba00@gmail.com>
2022-04-25 06:41:27 -0700

    Stoupper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -6,18 +6,18 @@ include "main.h"
  * Return: s modified.
  */
 
-char *string_toupper(char *s)
+char *string_toupper(char *str)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
+	while (str[i] != '\0')
 	{
-		if (s[i] >= 97  && s[i] <= 122)
+		if (str[i] >= 97  && str[i] <= 122)
 		{
-			s[i] = s[i] - 32;
+			str[i] = str[i] - 32;
 		}
 		i++;
 	}
 
-	return (s);
+	return (str);
 }
