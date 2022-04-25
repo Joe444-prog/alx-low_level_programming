b9c122ab5ec68a8533986f4015b48539b08febc7
Joe444-prog <Joshuagbenoba00@gmail.com>
2022-04-25 16:20:52 -0700

    T5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,25 +1,25 @@
-include "main.h"
+#include "main.h"
 
-/**
- * string_toupper - changes lowercase to uppercase.
- * @s: string to modify.
- * Return: s modified.
+/** 
+ * string_toupper - changes lowercase to uppercase 
+ * @str: the string to change 
+ * Return: the string that change for uppercase 
  */
 
 char *string_toupper(char *str)
-{
-	int i;
+{	
+	int i; 	
 	
-	i = 0;
-
-	while (str[i] != '\0')
-	{
-		if (str[i] >= 97  && str[i] <= 122)
-		{
-			str[i] = str[i] - 32;
-		}
-		i++;
-	}
-
+	i = 0; 	
+	while (str[i] != '\0')	
+	{		
+		if (str[i] >= 97 && str[i] <= 122)		
+		{			
+			str[i] = str[i] - 32;		
+		}		
+		
+		i++;	
+	}	
+	
 	return (str);
 }
