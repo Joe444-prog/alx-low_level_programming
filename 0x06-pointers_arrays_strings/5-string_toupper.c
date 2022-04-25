588d5d787afad65cda647698df3c7bdaa1161fc6
Joe444-prog <Joshuagbenoba00@gmail.com>
2022-04-25 06:38:30 -0700

    Stoupper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -12,9 +12,9 @@ char *string_toupper(char *s)
 
 	while (s[i] != '\0')
 	{
-		if (s[i] > 96 && s[i] < 123)
+		if (s[i] >= 97  && s[i] <= 122)
 		{
-			s[i] -= 32;
+			s[i] = s[i] - 32;
 		}
 		i++;
 	}
