5b8922026f3ccb9baed1e422cd6d2fb245b3a513
Joe444-prog <Joshuagbenoba00@gmail.com>
2022-05-15 14:24:36 -0700

    T0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "function_pointers.h"
 
 /**
@@ -8,7 +9,11 @@
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
+	{
 		return;
-
+	}
+	else
+	{
 	f(name);
+	}
 }
